32252c99b073078648f7ee148c916eb14b818ced
Cordis Stanfield <cordis.stanfield@gmail.com>
2014-07-26 16:54:43 -0400

    add admin boolean to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710054159) do
+ActiveRecord::Schema.define(version: 20140726205213) do
 
   create_table "user_details", force: true do |t|
     t.integer  "user_id"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20140710054159) do
     t.integer  "invitation_count"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
