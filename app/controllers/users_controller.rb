d71f05d8e33760769bd3a34e4004e4978255c189
Cordis Stanfield <cordis.stanfield@gmail.com>
2014-07-26 18:21:26 -0400

    make user_detail created by default when a user is created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #UserDetail.create(user_id: @user.id)
+      UserDetail.create(user_id: @user.id)
       cookies.permanent[:authentication_token] = @user.authentication_token 
       redirect_to edit_user_path(@user)
     else
