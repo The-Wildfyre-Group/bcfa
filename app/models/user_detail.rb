e8bf1379fdeccb8769ddc4f3f359235b83c9f03a
Cordis Stanfield <cordis.stanfield@gmail.com>
2014-09-07 23:00:08 -0400

    add a check to see if any social media links have been provided by the user

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,4 +1,8 @@
 class UserDetail < ActiveRecord::Base
   include Location
   belongs_to :user
+  
+  def social_media?
+    [twitter.present?, facebook.present?, linkedin.present?, instagram.present?].include? true
+  end
 end
