00b373d8985d36c860f69b674a6fb65e7fcc4bd8
Cordis Stanfield <cordis.stanfield@gmail.com>
2014-09-28 14:30:14 -0400

    make main/index charterholders dynamic

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,7 @@ class MainController < ApplicationController
   before_action :authenticate_user!, only: %[home]
   
   def index
+    @charterholders = User.where(level: "Charterholder")
     if current_user
       redirect_to home_path
     end 
