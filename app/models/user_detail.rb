95576faf0d615a60f29542a90c8b1e402c2ffad1
Cordis Stanfield <cordis.stanfield@gmail.com>
2014-10-02 18:55:58 -0400

    add 'Array' to serialize method to help with postgresql array errors

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -2,7 +2,7 @@ class UserDetail < ActiveRecord::Base
   include Location
   belongs_to :user
   
-  serialize :skills
+  serialize :skills, Array
   
   def social_media?
     [twitter.present?, facebook.present?, linkedin.present?, instagram.present?].include? true
