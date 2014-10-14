834e37c9539608ebf2355bcaa3eb718a9838b88b
Cordis Stanfield <cordis.stanfield@gmail.com>
2014-10-14 19:10:01 -0400

    fix signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserDetail.create(user_id: @user.id)
+      UserDetail.create(user_id: @user.id, skills: [])
       cookies.permanent[:authentication_token] = @user.authentication_token 
       redirect_to edit_user_path(@user)
     else
-      flash[:error] = @user.errors.full_messages
-      render 'new'
+      render text: @user.errors.full_messages
+      # flash[:error] = @user.errors.full_messages
+#       render 'new'
     end
   end
   
